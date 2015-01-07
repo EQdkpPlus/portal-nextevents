59e1f580398aed95a8aa5c0193cc46ead3f29e84
GodMod <GodMod@users.noreply.github.com>
2015-01-07 15:35:21 +0100

    bumped version to 2.0.0

--- nextevents_portal.class.php
+++ nextevents_portal.class.php
@@ -28,7 +28,7 @@ class nextevents_portal extends portal_generic {
 	protected static $path		= 'nextevents';
 	protected static $data		= array(
 		'name'			=> 'Next Events',
-		'version'		=> '1.1.0',
+		'version'		=> '2.0.0',
 		'author'		=> 'WalleniuM',
 		'icon'			=> 'fa-calendar-o',
 		'contact'		=> EQDKP_PROJECT_URL,
